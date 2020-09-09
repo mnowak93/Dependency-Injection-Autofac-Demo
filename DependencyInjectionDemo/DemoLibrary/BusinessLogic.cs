1e14776b7492d1ba9701cb2ebffbc046fe858134
Mateusz Nowak <nowak.mateusz.1993@gmail.com>
2020-09-09 23:21:30 +0200

    first commit

---
+++ DependencyInjectionDemo/DemoLibrary/BusinessLogic.cs
@@ -0,0 +1,28 @@
+﻿using DemoLibrary.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoLibrary
+{
+    public class BusinessLogic : IBusinessLogic
+    {
+        ILogger _logger;
+        IDataAccess _dataAccess;
+
+        public BusinessLogic(ILogger logger, IDataAccess dataAccess)
+        {
+            _logger = logger;
+            _dataAccess = dataAccess;
+        }
+
+        public void ProccesData()
+        {
+            _logger.Log("Starting the processing of data");
+            Console.WriteLine("Processing the data");
+            _dataAccess.LoadData();
+            _dataAccess.SaveData("Saving the data");
+            _logger.Log("Finishing processing of the data");
+        }
+    }
+}
---
+++ DependencyInjectionDemo/DependencyInjectionDemo/ContainerConfig.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public static class ContainerConfig
+    {
+        //Class for configuration of the autofac
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            //connecting the class to the interface
+            builder.RegisterType<BusinessLogic>().As<IBusinessLogic>();
+            builder.RegisterType<Application>().As<IApplication>();
+
+            builder.RegisterAssemblyTypes(Assembly.Load(nameof(DemoLibrary)))
+                .Where(t => t.Namespace.Contains("Utilities"))
+                .As(t => t.GetInterfaces().FirstOrDefault(i => i.Name == "I" + t.Name));
+
+            return builder.Build();
+        }
+    }
+}
